edd217175b18522d6e6ad059678c2b68b7e1e98e
VMekh8 <mekhvitaliy88@gmail.com>
2023-09-13 18:55:01 +0300

    Create and Design Home Button

--- Core/ObesrvalbleObject.cs
+++ Core/ObesrvalbleObject.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace ModernDesign.Core
 {
-    class ObesrvalbleObject
+    class ObesrvalbleObject : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
     }
 }
