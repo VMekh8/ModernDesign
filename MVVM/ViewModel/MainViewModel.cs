00481a4bb2fd1ed73c961bd48167a374e045fcf1
VMekh8 <mekhvitaliy88@gmail.com>
2023-09-14 18:45:35 +0300

    Make a switcher between the user contorls

--- MVVM/ViewModel/MainViewModel.cs
+++ MVVM/ViewModel/MainViewModel.cs
@@ -9,8 +9,31 @@ namespace ModernDesign.MVVM.ViewModel
 {
     class MainViewModel : ObesrvalbleObject
     {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+
+
+        public HomeViewModel HomeVM { get; set; }
+        public DiscoveryViewModel DicoveryVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
         public MainViewModel()
         {
+            HomeVM = new HomeViewModel();
+            DicoveryVM = new DiscoveryViewModel();
+            CurrentView = HomeVM;
+            HomeViewCommand = new RelayCommand(o => { CurrentView = HomeVM; });
+            DiscoveryViewCommand = new RelayCommand(x => { CurrentView = DicoveryVM; });
 
         }
     }
